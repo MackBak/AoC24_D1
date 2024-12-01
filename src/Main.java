0e884793c216b7803479ecf7e6295dcf51bdc0b8
MackB <mackbakkum@gmail.com>
2024-12-01 17:58:31 +0100

    Finalized part 1

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         String csvFile = "src/data/inputdata.csv";
-
-        ArrayList<Integer> leftArray = new ArrayList<Integer>();
-        ArrayList<Integer> rightArray = new ArrayList<Integer>();
-
+        ArrayList<Integer> leftArray = new ArrayList<>();
+        ArrayList<Integer> rightArray = new ArrayList<>();
         csvReader reader = new csvReader(csvFile);
         reader.csvParse(leftArray, rightArray);
 
-        System.out.println("######## Left Numbers Array ########\n");
-        for (Integer num : leftArray) {
-            System.out.print(num + "\n");
-        }
-
-
-        System.out.println("######## Sorting done: ########\n" +
-                "#\n#\n#\n#\n#\n#############");
+        // Sorting:
         Collections.sort(leftArray);
         Collections.sort(rightArray);
 
-        for (Integer num : leftArray) {
-            System.out.print(num + "\n");
-        }
-            }
+        // Check size
+        System.out.println(leftArray.size()); // 1000
+        System.out.println(rightArray.size()); // 1000
+
+        // Calculation
+        long sum = calculator.calculateDifference(leftArray, rightArray);
+        System.out.println(sum);
+
+
+    }
 }
---
+++ src/calculator.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class calculator {
+
+        public static Long calculateDifference(ArrayList<Integer> leftArray, ArrayList<Integer> rightArray) {
+
+            long sum = 0;
+            int iterations = leftArray.size();
+            System.out.println("GOing to iterate " + iterations + " amount of times");
+
+            for (int i = 0; i < iterations; i++) {
+                int leftNumber = leftArray.get(i);
+                int rightNumber = rightArray.get(i);
+                int difference = Math.abs(leftNumber - rightNumber);
+                sum = sum + difference;
+            }
+            return sum;
+        }
+}

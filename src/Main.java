2c4796ae5920247cdd55b94deff6b89deaa6a8e6
MackB <mackbakkum@gmail.com>
2024-12-01 17:45:34 +0100

    With collection sort

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -17,5 +18,14 @@ public class Main {
             System.out.print(num + "\n");
         }
 
-    }
+
+        System.out.println("######## Sorting done: ########\n" +
+                "#\n#\n#\n#\n#\n#############");
+        Collections.sort(leftArray);
+        Collections.sort(rightArray);
+
+        for (Integer num : leftArray) {
+            System.out.print(num + "\n");
+        }
+            }
 }
